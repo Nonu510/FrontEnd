7ba8c676a440037a6324080cb7e1e45deeeb6b38
Nolwenn Dubourg <johndoe@example.com>
2025-06-06 18:28:55 +0200

    ajout des projets dynamiquement et suppression du HTML correspondant

--- assets/projet.js
+++ assets/projet.js
@@ -1,23 +1,40 @@
+// Récuperation des projets depuis l'API
 const gallery = document.querySelector(".gallery")
 
 async function recupererprojet () { 
     const url = "http://localhost:5678/api/works";
     try{
         const response = await fetch(url);
+        const works = await response.json();
+        actualisergallery(works);
         if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
-    const json = await response.json();
-    console.log(json);
      } catch (error) {
     console.error(error.message);
   }
 }
 
-function actualisergallery () {
-    array.forEach(element => {
+// Boucle sur pour chaque iteration crée une image avec son alt et titre 
+function actualisergallery(works) {
+    for (let i = 0; i < works.length; i++) {
+      const work = works[i];
+      const figure = document.createElement("figure");
+      const img = document.createElement("img");
+      img.src = work.imageUrl;
+      img.alt = work.title;
+
+      const caption = document.createElement("figcaption");
+      caption.innerText = work.title;
+
+      figure.appendChild(img);
+      figure.appendChild(caption);
+      gallery.appendChild(figure);
+    }
         
-    });
-}
+    };
+
+    recupererprojet ();
+
 
 
