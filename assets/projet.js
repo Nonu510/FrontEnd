209158e2680998b739858f856d9e9a786c567d18
Nolwenn Dubourg <johndoe@example.com>
2025-06-10 12:26:42 +0200

    CSS des boutons filtres et JS des filtres

--- assets/projet.js
+++ assets/projet.js
@@ -1,11 +1,13 @@
 // Récuperation des projets depuis l'API
 const gallery = document.querySelector(".gallery")
+let allWorks = []
 
 async function recupererprojet () { 
     const url = "http://localhost:5678/api/works";
     try{
         const response = await fetch(url);
         const works = await response.json();
+        allWorks = works;
         actualisergallery(works);
         if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
@@ -17,6 +19,7 @@ async function recupererprojet () {
 
 // Boucle sur pour chaque iteration crée une image avec son alt et titre 
 function actualisergallery(works) {
+    gallery.innerHTML = "";
     for (let i = 0; i < works.length; i++) {
       const work = works[i];
       const figure = document.createElement("figure");
@@ -34,7 +37,38 @@ function actualisergallery(works) {
         
     };
 
-    recupererprojet ();
+async function fetchCategories() {
+  try {
+    const response = await fetch("http://localhost:5678/api/categories");
+    const categories = await response.json();
+    filtrecategories(categories);
+  } catch (error) {
+    console.error("Erreur lors de la récupération des catégories :", error);
+  }
+}
 
 
+function filtrecategories(categories) {
+    const filtres = document.querySelector(".filtres");
+    const tous = document.createElement("button");
+    tous.innerText = "Tous";
+    tous.classList.add("btn");
+    tous.classList.add("tous")
+    tous.addEventListener("click", () => actualisergallery(allWorks));
+    filtres.appendChild(tous);
 
+for (let index = 0; index < categories.length; index++) {
+    const category = categories[index];
+    const button = document.createElement("button");
+    button.innerText = category.name;
+    button.classList.add("btn");
+    button.addEventListener("click", () => {
+      const filteredworks = allWorks.filter(work => work.categoryId === category.id);
+      actualisergallery(filteredworks);
+    });
+    filtres.appendChild(button)
+  }
+}
+
+fetchCategories()
+recupererprojet ();
